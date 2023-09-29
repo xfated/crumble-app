039f737aa1cee2d8e08c875ea2e1e780112a60db
Kai Yang <taykaiyang556@gmail.com>
2023-09-29 21:58:15 +0800

    update changes

--- test_build.sh
+++ test_build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Building apk for android
-eas build -p android  --profile preview
+# eas build -p android  --profile preview
 
 # Submit to ios
-eas build -p ios 
-eas submit -p ios
+# eas build -p ios 
+# eas submit -p ios
 
 # Submit to android
 eas build -p android
